f09797021ee8e8f48d12e8750d4a5c8d4b685b51
Stephen Early <steve@assorted.org.uk>
2026-05-25 18:30:52 +0100

    Serve media files in development

--- tillweb/config/urls.py
+++ tillweb/config/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -38,3 +40,7 @@ urlpatterns = [
     # path('', views.index, name="frontpage"),
     path('', include(emf.urls.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
