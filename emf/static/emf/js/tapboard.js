2eb6a3da3e2cc793e79233dd00be79db501ec50e
Tom Lynch <me@unknowndomain.co.uk>
2026-06-02 14:58:12 +0100

    First re-hash of the tapboards

--- emf/static/emf/js/tapboard.js
+++ emf/static/emf/js/tapboard.js
@@ -4,26 +4,26 @@
 const setup = document.getElementById("setup");
 const run = document.getElementById("run");
 const linename = document.getElementById("linename");
-const logo = document.getElementById("logo");
+const logo = document.querySelector(".logo");
 const tastingNotesContainer = document.getElementById("tastingNotesContainer");
 const tastingNotes = document.getElementById("tastingNotes");
 const noteFormContainer = document.getElementById("noteFormContainer");
 const noteInput = document.getElementById("noteInput");
-const product = document.getElementById("product");
-const price = document.getElementById("price");
-const linenote = document.getElementById("linenote");
+const product = document.querySelector(".product");
+const price = document.querySelector(".price");
+const linenote = document.querySelector(".linenote");
 const notebutton = document.getElementById("notebutton");
-const status = document.getElementById("status");
 const setupStatus = document.getElementById("setupStatus");
 const setupForm = document.getElementById("setupForm");
 const stocklineSelect = document.getElementById("stocklineSelect");
 const notePassword = document.getElementById("notePassword");
+const totalStock = document.querySelector(".totalStock");
+const connectedStock = document.querySelector(".connectedStock");
+
 
 /* The default HTML for the page includes the "not connected" logo */
-const notConnectedLogo = logo.innerHTML;
-const notConnectedStatus = status.innerText;
-
-const idleLogo = document.getElementById("idleLogo").innerHTML;
+const idleLogo = document.querySelector(".logo").style.backgroundImage;
+const notConnectedStatus = linenote.innerText;
 
 const recoverTime = 5000; /* How long to wait after network error? */
 
@@ -38,25 +38,65 @@ let password = null;
 let socket = null;
 let running = false;
 
+
+// On page load, set up event handlers for the various buttons and interactive elements on the page.
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Show menu
+    menubutton.addEventListener("click", setup_mode);
+
+    // Show tasting notes
+    logo.addEventListener("click", toggle_tasting_notes);
+    tastingNotesContainer.addEventListener("click", show_logo);
+
+    // Show notes form
+    notebutton.addEventListener("click", show_note_form);
+    noteFormContainer.querySelector(".cancel").addEventListener("click", show_logo);
+
+    // Handle set note buttons
+    document.querySelectorAll(".setNote").forEach((button) => {
+        button.addEventListener("click", (e) => {
+            e.preventDefault();
+            const note = button.innerText;
+            set_note(note);
+        });
+    });
+
+    // Handle update note button
+    document.querySelector(".updateNote").addEventListener("click", (e) => {
+        e.preventDefault();
+        set_note();
+    });
+
+    // Handle development tap sequence
+    document.querySelectorAll("header h1").forEach((h1) => {
+        h1.addEventListener("click", _dev_tap);
+    });
+
+});
+
 /* Utilities — prevent flicker when nothing has changed */
 function updateHTML(element, contents) {
     if (element.innerHTML != contents) {
-	element.innerHTML = contents;
+    	element.innerHTML = contents;
     }
 }
 
 function updateText(element, contents) {
     if (element.innerText != contents) {
-	element.innerText = contents;
+	    element.innerText = contents;
     }
 }
 
 /* "Run" mode */
-function show_tasting_notes() {
+function toggle_tasting_notes() {
     if (tastingNotes.innerHTML) {
-	logo.classList.add("d-none");
-	noteFormContainer.classList.add("d-none");
-	tastingNotesContainer.classList.remove("d-none");
+        if (tastingNotesContainer.classList.contains("d-none")) {
+            tastingNotesContainer.classList.remove("d-none");
+            noteFormContainer.classList.add("d-none");
+        } else {
+            show_logo();
+        }
     }
 }
 
@@ -67,9 +107,12 @@ function show_logo() {
 }
 
 function show_note_form() {
-    tastingNotesContainer.classList.add("d-none");
-    logo.classList.add("d-none");
-    noteFormContainer.classList.remove("d-none");
+    if (noteFormContainer.classList.contains("d-none")) {
+        tastingNotesContainer.classList.add("d-none");
+        noteFormContainer.classList.remove("d-none");
+    } else {
+        show_logo();
+    }
 }
 
 function set_note(note) {
@@ -77,80 +120,96 @@ function set_note(note) {
     show_logo();
     noteInput.value = "";
     if (stockline_id === null) {
-	return false;
+        return false;
     }
     fetch(`/api/stockline/${stockline_id}/set-note/`, {
-	method: "POST",
-	headers: {
-	    "Content-Type": "application/json",
-	},
-	body: JSON.stringify({
-	    password: password,
-	    note: new_note,
-	}),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            password: password,
+            note: new_note,
+        }),
     });
     return false;
 }
 
 function not_connected_message() {
-    updateText(status, notConnectedStatus);
-    updateHTML(logo, notConnectedLogo);
+    updateText(linenote, notConnectedStatus);
+    logo.style.backgroundImage = idleLogo;
     /* Leave the line name up if present */
+    updateText(totalStock, "");
+    updateText(connectedStock, "");
     updateText(product, "");
     updateText(price, "");
-    updateHTML(linenote, "");
 }
 
 function process_message(message) {
     m = JSON.parse(message);
     if (m.type == "error") {
-	/* Go back to setup mode and try again */
-	setup_mode();
+	    /* Go back to setup mode and try again */
+	    setup_mode();
     } else if (m.type == "not present") {
-	/* The server is probably restarting; leave things as they are
-	   for now but note in the status line */
-	stockline_id = null;
-	updateText(status, "(No data received from server; waiting...)");
+        /* The server is probably restarting; leave things as they are
+        for now but note in the status line */
+        stockline_id = null;
+        updateText(linenote, "(No data received from server; waiting...)");
     } else if (m.type == "stockline") {
-	/* Normal expected response */
-	updateText(linename, m.name);
-	stockline_id = m.id;
-	if (m.stockitem === null) {
-	    /* There's nothing connected to the line right now */
-	    show_logo();
-	    updateHTML(logo, idleLogo);
-	    updateHTML(tastingNotes, "");
-	    updateText(product, "");
-	    updateText(price, "");
-	    updateHTML(linenote, `<p>${m.note}</p>`);
-	    updateText(status, "No product connected");
-	} else {
-	    if (m.stockitem.stocktype.logo) {
-		updateHTML(logo, `<a onclick="show_tasting_notes();"><img src="${m.stockitem.stocktype.logo}" alt=""></a>`);
-	    } else {
-		updateHTML(logo, "");
-	    }
-	    if (m.stockitem.stocktype.tasting_notes) {
-		updateHTML(tastingNotes, `<p>${m.stockitem.stocktype.tasting_notes}</p>`);
-	    } else {
-		updateHTML(tastingNotes, "");
-		show_logo();
-	    }
-	    /* The space in '% ABV' is replaced with a non-breaking space
-	       to improve how this looks on narrow displays */
-	    updateText(product, m.stockitem.stocktype.fullname.replace(
-		"% ABV", "% ABV"));
-	    updateText(price, `£${m.stockitem.stocktype.price}/${m.stockitem.stocktype.sale_unit_name}`);
-	    if (m.note) {
-		updateHTML(linenote, `<p><div class="caution"></div> ${m.note}</p>`);
-	    } else {
-		updateHTML(linenote, "");
-	    }
-	    updateText(status, `Connected: ${m.stockitem.remaining}/${m.stockitem.size} ${m.stockitem.stocktype.base_unit_name}s; total: ${m.stockitem.stocktype.base_units_remaining}/${m.stockitem.stocktype.base_units_bought} ${m.stockitem.stocktype.base_unit_name}s`);
-	}
+        /* Normal expected response */
+        updateText(linename, m.name);
+        stockline_id = m.id;
+
+        if (m.stockitem === null) {
+            /* There's nothing connected to the line right now */
+            show_logo();
+            logo.style.backgroundImage = idleLogo;
+            updateHTML(tastingNotes, "");
+            updateText(product, "");
+            updateText(price, "");
+            updateHTML(linenote, m.note);
+            updateText(status, "No product connected");
+        } else {
+            if (m.stockitem.stocktype.logo) {
+                logo.style.backgroundImage = `url(${m.stockitem.stocktype.logo})`;
+            } else {
+                logo.style.backgroundImage = `url(${idleLogo})`;
+            }
+
+            if (m.stockitem.stocktype.tasting_notes) {
+                updateHTML(tastingNotes, m.stockitem.stocktype.tasting_notes);
+            } else {
+                updateHTML(tastingNotes, "");
+                show_logo();
+            }
+
+            /* The space in '% ABV' is replaced with a non-breaking space
+            to improve how this looks on narrow displays */
+            updateText(product, m.stockitem.stocktype.fullname.replace("% ABV", "% ABV"));
+            updateText(price, `£${m.stockitem.stocktype.price}/${m.stockitem.stocktype.sale_unit_name}`);
+
+
+            if (m.note) {
+                linenote.classList.add("caution");
+            } else {
+                linenote.classList.remove("caution");
+            }
+            updateText(linenote, m.note);
+
+            let connectedStockData = m.stockitem;
+            let totalStockData = m.stockitem.stocktype;
+
+            let connectedStockRemaining = parseInt(connectedStockData.remaining);
+            let connectedStockSize = parseInt(connectedStockData.size);
+            let totalStockRemaining = parseInt(totalStockData.base_units_remaining);
+            let totalStockBought = parseInt(totalStockData.base_units_bought);
+
+            updateText(connectedStock, `Connected: ${connectedStockRemaining} / ${connectedStockSize} ${connectedStockData.stocktype.base_unit_name}s`);
+            updateText(totalStock, `Total: ${totalStockRemaining} / ${totalStockBought} ${totalStockData.base_unit_name}s`);
+        }
     } else {
-	/* Unknown message type */
-	updateText(status, `Unknown message type ${m.type} received!`);
+        /* Unknown message type */
+        updateText(linenote, `Unknown message type ${m.type} received!`);
     }
 }
 
@@ -161,28 +220,35 @@ function subscribe() {
 function run_mode() {
     running = true;
     setup.classList.add("d-none");
+
     if (!password) {
-	/* Hide the "Problem?" note button if we don't have a password */
-	notebutton.classList.add("d-none");
+        /* Hide the "Problem?" note button if we don't have a password */
+        notebutton.classList.add("d-none");
     } else {
-	notebutton.classList.remove("d-none");
+        notebutton.classList.remove("d-none");
     }
+
     run.classList.remove("d-none");
 
     socket = new WebSocket(websocket_address);
+
     socket.addEventListener("open", subscribe);
+
     socket.addEventListener("message", (event) => {
-	process_message(event.data);
+	    process_message(event.data);
     });
+
     socket.addEventListener("error", (event) => {
-	console.log("websocket error");
+        console.log("websocket error");
     });
+
     socket.addEventListener("close", (event) => {
-	socket = null;
-	not_connected_message();
-	if (running) {
-	    setTimeout(run_mode, recoverTime);
-	}
+        socket = null;
+        not_connected_message();
+
+        if (running) {
+            setTimeout(run_mode, recoverTime);
+        }
     });
 }
 
@@ -190,6 +256,8 @@ function run_mode() {
 
 async function setup_mode() {
     let stocklines = null;
+
+    show_logo();
 
     running = false;
     run.classList.add("d-none");
@@ -199,24 +267,26 @@ async function setup_mode() {
 
     /* Make sure websocket is closed down */
     if (socket !== null) {
-	socket.close();
+	    socket.close();
     }
 
     /* Fetch list of stocklines and set up form element */
     try {
-	const response = await fetch("/api/stocklines.json?type=regular");
-	stocklines = await response.json();
+        const response = await fetch("/api/stocklines.json?type=regular");
+        stocklines = await response.json();
     } catch (error) {
-	console.error(error);
-	setupStatus.innerText = "Failed to fetch list of stock lines. Are we offline?";
-	return;
+        console.error(error);
+        setupStatus.innerText = "Failed to fetch list of stock lines. Are we offline?";
+        return;
     }
+
     const options = stocklines.stocklines.map((x) => {
-	o = document.createElement("option");
-	o.innerText = x.name;
-	o.value = x.key;
-	return o;
+        o = document.createElement("option");
+        o.innerText = x.name;
+        o.value = x.key;
+        return o;
     });
+
     stocklineSelect.replaceChildren(...options);
     stocklineSelect.value = stockline;
     notePassword.value = password;
@@ -242,24 +312,24 @@ if ("wakeLock" in navigator) {
     let wakeLock = null;
 
     async function acquire_wakelock() {
-	try {
-	    wakeLock = await navigator.wakeLock.request("screen");
-	    wakeLock.addEventListener("release", () => {
-		wakeLock = null;
-	    });
-	} catch (err) {
-	    console.log("failed to acquire wakelock", err);
-	}
+        try {
+            wakeLock = await navigator.wakeLock.request("screen");
+            wakeLock.addEventListener("release", () => {
+            wakeLock = null;
+            });
+        } catch (err) {
+            console.log("failed to acquire wakelock", err);
+        }
     }
 
     (async () => {
-	await acquire_wakelock();
+        await acquire_wakelock();
 
-	document.addEventListener("visibilitychange", async () => {
-	    if (wakeLock === null && document.visibilityState === "visible") {
-		await acquire_wakelock();
-	    }
-	});
+        document.addEventListener("visibilitychange", async () => {
+            if (wakeLock === null && document.visibilityState === "visible") {
+                await acquire_wakelock();
+            }
+        });
     })();
 }
 
@@ -303,9 +373,9 @@ function init() {
     stockline = localStorage.getItem(lsStocklineKey);
     password = localStorage.getItem(lsNotePassword);
     if (stockline === null) {
-	setup_mode();
+	    setup_mode();
     } else {
-	run_mode();
+	    run_mode();
     }
 }
 
